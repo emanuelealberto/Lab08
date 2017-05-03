aa54b804b76e64097593a13949538054bc409aa9
Emanuele <Emanuele@Emanuele>
2017-05-03 15:15:16 +0200

    problemi tecnici

--- Lab08_CountryBorders/src/it/polito/tdp/borders/model/Model.java
+++ Lab08_CountryBorders/src/it/polito/tdp/borders/model/Model.java
@@ -21,24 +21,34 @@ public class Model {
 	public String createGraph(int anno) throws SQLException {
 		
 		countries.addAll(bd.loadAllCountries());
-		for (Country c : countries){
+		/*for (Country c : countries){
 			graph.addVertex(c);
-		}
+		}*/
 		List<Border> borders = new ArrayList<Border>();
 		//List<Border> borders2 = new ArrayList<Border>();
 		//System.out.println();
 		borders.addAll(bd.getCountryPairs(anno));
-		System.out.println(borders.toString());
+		/*System.out.println(borders.toString());
 		for (Border b : borders){
 			if(countries.contains(b.getC1())&&countries.contains(b.getC2())){
 				graph.addEdge(b.getC1(), b.getC2());
 				/*bo.setC1(c1);
-				bo.setC2(c2);*/
+				bo.setC2(c2);
 				//borders2.add(bo);
 				}
 			
-		}
+		}*/
 		//System.out.println(graph);
+		for(Border b : borders){
+			Country c1 = b.getC1();
+			Country c2 = b.getC2();
+			if(!graph.vertexSet().contains(c1))
+				graph.addVertex(c1);
+			if(!graph.vertexSet().contains(c2))
+				graph.addVertex(c2);
+			if(!graph.edgeSet().contains(b))
+				graph.addEdge(c1, c2, b);
+}
 		return graph.toString();
 	}
 
